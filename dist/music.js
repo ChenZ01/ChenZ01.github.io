b7c57a73964c9b4f17aabb277b4b4ff1c5ebbf03
ChenZ01 <Chen2016MS@outlook.com>
2018-08-27 22:19:38 +0800

    Add&Update

--- dist/music.js
+++ dist/music.js
@@ -4,7 +4,8 @@ const ap = new APlayer({
     audio: [{
         name: 'Date a Live Piano Version',
         artist: '坂部剛',
-        url: 'https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcEg4NmkyQU9zWU5rbG1SYnQ1SHU5elNfSFNo.mp3',
+
+        url: 'https://m10.music.126.net/20180827091103/df4c53fbc26261e64638149cbcb74c0e/ymusic/5057/2e7f/6ba0/c14aa10665a953e424f19959e3d59715.mp3',
         cover: 'http://p1.music.126.net/4OinmWWGjtCgT56fEgSeig==/5727356069143337.jpg'
     }]
 });
