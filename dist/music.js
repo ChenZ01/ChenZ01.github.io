95c12e0889248b2618be2234dd7eba013a859497
ChenZ01 <Chen2016MS@outlook.com>
2018-09-02 06:39:10 +0800

    Add&Fix&Change

--- dist/music.js
+++ dist/music.js
@@ -5,7 +5,7 @@ const ap = new APlayer({
         name: 'Date a Live Piano Version',
         artist: '坂部剛',
 
-        url: 'https://m10.music.126.net/20180827091103/df4c53fbc26261e64638149cbcb74c0e/ymusic/5057/2e7f/6ba0/c14aa10665a953e424f19959e3d59715.mp3',
+        url: 'http://t.cn/RF0Rgw3?mp3',
         cover: 'http://p1.music.126.net/4OinmWWGjtCgT56fEgSeig==/5727356069143337.jpg'
     }]
 });
